b1ed0408af306dbd865cc0e9bfec6229e1a777c8
NickRayF <niklpn11@gmail.com>
2026-08-08 13:26:01 +0300

    feat: delete useless cart

--- frontend/src/config/paths.ts
+++ frontend/src/config/paths.ts
@@ -1,6 +1,5 @@
 export const PATHS = {
   HOME: '/',
-  CART: '/cart',
   AVITO: '/avito',
   AVITO_ORIGINAL: "https://www.avito.ru/"
 } as const
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,14 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
-
 import { PATHS } from '@/config/paths'
-import { CartPage } from '@/pages/Cart'
 import { HomePage } from '@/pages/Home'
 import { AvitoPage } from '@/pages/Avito'
 
 export function AppRouter() {
   const navigations = [
     { path: PATHS.HOME, element: <HomePage /> },
-    { path: PATHS.CART, element: <CartPage /> },
     { path: `${PATHS.AVITO}/*`, element: <AvitoPage /> },
   ] as const
 
